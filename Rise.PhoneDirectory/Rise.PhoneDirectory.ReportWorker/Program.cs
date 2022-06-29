4bc938d160ab6954fc69edd349f24d3e6b4ac582
Serkan Doğanbaş <serkandoganbas@hotmail.com.tr>
2022-06-29 14:35:43 +0300

    - Create project base.

---
+++ Rise.PhoneDirectory/Rise.PhoneDirectory.ReportWorker/Program.cs
@@ -0,0 +1,10 @@
+using Rise.PhoneDirectory.ReportWorker;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
