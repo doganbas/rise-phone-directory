d866ee5b8cadb7db00047010b44ad0d809131ee0
Serkan Doğanbaş <serkandoganbas@hotmail.com.tr>
2022-06-30 14:47:51 +0300

    - Refactoring Report Controller, Report Service. - Remove Report Repository.

--- Rise.PhoneDirectory/Rise.PhoneDirectory.Core/Services/IReportService.cs
+++ Rise.PhoneDirectory/Rise.PhoneDirectory.Core/Services/IReportService.cs
@@ -1,16 +1,62 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Rise.PhoneDirectory.Store.Dtos;
 using Rise.PhoneDirectory.Store.Models;
+using System.Linq.Expressions;
 
 namespace Rise.PhoneDirectory.Core.Services
 {
-    public interface IReportService : IGenericService<Report>
+    public interface IReportService
     {
+        Task<ReportDto> GetByIdAsync(int id);
+
+        ReportDto GetById(int id);
+
+
+        IEnumerable<ReportDto> Where(Expression<Func<Report, bool>> expression = null);
+
+
+        Task<bool> AnyAsync(Expression<Func<Report, bool>> expression = null);
+
+        bool Any(Expression<Func<Report, bool>> expression = null);
+
+
+        Task<ReportDto> AddAsync(ReportDto entity);
+
+        ReportDto Add(ReportDto entity);
+
+
+        Task<IEnumerable<ReportDto>> AddRangeAsync(IEnumerable<ReportDto> entities);
+
+        IEnumerable<ReportDto> AddRange(IEnumerable<ReportDto> entities);
+
+
+        Task UpdateAsync(ReportDto entity);
+
+        void Update(ReportDto entity);
+
+
+        Task RemoveAsync(ReportDto entity);
+
+        void Remove(ReportDto entity);
+
+
+        Task RemoveAsync(int id);
+
+        void Remove(int id);
+
+
+        Task RemoveRageAsync(IEnumerable<ReportDto> entities);
+
+        void RemoveRage(IEnumerable<ReportDto> entities);
+
+
+        public List<ReportDataDto> GetReportData();
+
+
         public Task<bool> ReportExcelAsync(int reportId);
 
         public bool ReportExcel(int reportId);
 
-        public List<ReportDataDto> GetReportData();
 
         public Task<bool> CompleteReportAsync(IFormFile reportFile, int reportId);
 
